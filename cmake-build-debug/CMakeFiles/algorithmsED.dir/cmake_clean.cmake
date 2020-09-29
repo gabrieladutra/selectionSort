31f43ee9109f2ba9764a030ff161a9778dc7d6ba
Gabriela DIas Dutra <gabriela_ddutra@outlook>
2020-09-28 21:15:59 -0300

    First commit

---
+++ cmake-build-debug/CMakeFiles/algorithmsED.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithmsED.dir/main.c.o"
+  "algorithmsED"
+  "algorithmsED.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/algorithmsED.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
